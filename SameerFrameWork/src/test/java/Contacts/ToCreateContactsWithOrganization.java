d08b0aafdb863709907b7e820519611c6ee99a26
samee <samee@DESKTOP-4VL1BVC>
2024-11-07 15:19:57 +0530

    i have shared my framework

---
+++ SameerFrameWork/src/test/java/Contacts/ToCreateContactsWithOrganization.java
@@ -0,0 +1,9 @@
+package Contacts;
+
+public class ToCreateContactsWithOrganization {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ SameerFrameWork/src/test/java/Organization/ToCreateOrgWithIndusrty.java
@@ -0,0 +1,8 @@
+package Organization;
+
+public class ToCreateOrgWithIndusrty {
+	public static void main(String[] args) {
+		
+	}
+
+}
